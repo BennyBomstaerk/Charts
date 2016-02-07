c64f9bd32fb9fa47870aaa912f6d39fee6e36a7f
Benny Boye Johansen <skukbear@hotmail.com>
2016-02-07 18:55:37 +0100

    First TypeScript file

---
+++ Charts/openapiconfig.ts
@@ -0,0 +1,10 @@
+﻿var getOpenApiConfig = function (baseUrl) {
+
+    return {
+        BASE_URL: baseUrl,
+        STREAMING_CONNECTION_URL: baseUrl + "/streaming/connection",
+        STREAMING_KEEP_ALIVE_URL: baseUrl + "/root/subscriptions/keepalive",
+        CHART_URL: baseUrl + "/chart/v1/charts",
+        SUBSCRIPTION_URL: baseUrl + "/chart/v1/charts/subscriptions"
+    }
+}
